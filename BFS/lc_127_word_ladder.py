15cbc282e50d2c54495b2c6be96b98c26875472f
JeffCX <xc1008@nyu.edu>
2020-04-27 11:18:48 -0400

    Update lc_127_word_ladder.py

--- BFS/lc_127_word_ladder.py
+++ BFS/lc_127_word_ladder.py
@@ -1,31 +1,26 @@
 from queue import Queue
 
 class Solution:
-
     def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
         
-        if not beginWord or not endWord:
+        if not beginWord or not wordList:
             return 0
         
-        def create_char_dic():
+        # Queue
+        q = Queue()
+        
+        # create char list
+        def create_char_list():
             char_list = []
+            ascii_code = ord("a")
             for i in range(26):
-                ascii_code = ord("a")
                 char_list.append(chr(ascii_code+i))
             return char_list
         
-        char_list = create_char_dic()
-        q = Queue()
-        q.put((beginWord,1))
+        char_list = create_char_list()
+        q.put((beginWord, 1))
         visited = set()
-        
-        def bfs(word_list, level):
-            for char in char_list:
-                    word_list[i] = char
-                    new_word = "".join(word_list)
-                    if new_word not in visited and new_word in wordList:
-                        visited.add(new_word)
-                        q.put((new_word, level+1))
+        word_set = set(wordList)
         
         while not q.empty():
             
@@ -37,8 +32,17 @@ class Solution:
             word_list = list(word)
             
             for i in range(len(word_list)):
+                
                 temp_char = word_list[i]
-                bfs(word_list, level)
-                word_list[i] = temp_char
+                
+                for char in char_list:
                     
+                    word_list[i] = char
+                    new_word = "".join(word_list)
+                    if new_word not in visited and new_word in word_set:
+                        visited.add(new_word)
+                        q.put((new_word, level + 1))
+                        
+                word_list[i] = temp_char
+            
         return 0
